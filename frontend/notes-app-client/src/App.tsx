32a09d2ac8d07fa1ac3ddf0fce856e87bccd41c6
janak-Thapa <ogjanak1@gmail.com>
2024-06-01 13:33:11 +0545

    changes

--- frontend/notes-app-client/src/App.tsx
+++ frontend/notes-app-client/src/App.tsx
@@ -1,24 +1,19 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Corrected import
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/Signup';
 
-const routes = (
-  <Router>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-    </Routes>
-  </Router>
-);
-
 const App = () => {
   return (
     <div>
-      {routes}
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
