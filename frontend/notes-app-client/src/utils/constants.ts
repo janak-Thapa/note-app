42e05a6b602f83a7d075b19fc11711d2ab7f8403
janak-Thapa <ogjanak1@gmail.com>
2024-06-01 11:55:06 +0545

    change in base url

--- frontend/notes-app-client/src/utils/constants.ts
+++ frontend/notes-app-client/src/utils/constants.ts
@@ -1,12 +1,4 @@
-let base_url = '';
-
-// Check if process is defined (Node.js environment)
-if (typeof process !== 'undefined') {
-    base_url = process.env.BASE_URL || '';
-}
-
 const _config = {
-    base_url: base_url
+    base_url: process.env.BASE_URL
 };
-
 export const constant = Object.freeze(_config);
