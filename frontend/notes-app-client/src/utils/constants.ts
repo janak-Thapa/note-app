1e9f928e92bcd83ebfc6a6cab1ebef0c2ab2a24c
janak-Thapa <ogjanak1@gmail.com>
2024-06-01 11:31:48 +0545

    add favicon

--- frontend/notes-app-client/src/utils/constants.ts
+++ frontend/notes-app-client/src/utils/constants.ts
@@ -1,4 +1,12 @@
+let base_url = '';
+
+// Check if process is defined (Node.js environment)
+if (typeof process !== 'undefined') {
+    base_url = process.env.BASE_URL || '';
+}
+
 const _config = {
-    base_url: process.env.BASE_URL
+    base_url: base_url
 };
+
 export const constant = Object.freeze(_config);
