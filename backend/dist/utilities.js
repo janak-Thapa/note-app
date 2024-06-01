1cccfe1126d2d2c5d41a130650f41ffc83afd7e2
janak-Thapa <ogjanak1@gmail.com>
2024-06-01 10:17:35 +0545

    add dist folder

---
+++ backend/dist/utilities.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const config_1 = require("./src/config/config");
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.sendStatus(401); // Unauthorized
+    }
+    jsonwebtoken_1.default.verify(token, config_1.config.jwtSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403); // Forbidden
+        }
+        req.user = { userId: user.userId };
+        next();
+    });
+};
+exports.default = authenticateToken;
